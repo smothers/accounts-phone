7ce9fb3734e0945d278d17ed562bb52571dd66e2
Chris Smothers <chris.smothers@gmail.com>
2017-03-28 11:20:10 -0400

    bump bcrypt

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name         : 'smothers:accounts-phone',
-    version      : '0.0.25',
+    version      : '0.0.26',
     // Brief, one-line summary of the package.
     summary      : 'A login service based on mobile phone number, For Meteor.',
     // URL to the Git repository containing the source code for this package.
@@ -17,7 +17,7 @@ Npm.depends({
 });
 
 Package.onUse(function (api) {
-    api.use('npm-bcrypt@=0.8.6_3', 'server');
+    api.use('npm-bcrypt@=0.9.2', 'server');
 
     api.use('accounts-base@1.0.2', ['client', 'server']);
     // Export Accounts (etc) to packages using this one.
